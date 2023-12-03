65152816b5af9403a074ddc31f39be82cdd00f62
PaoCalle <144401466+PaoCalle@users.noreply.github.com>
2023-12-03 09:31:57 -0500

    Add files via upload

---
+++ preguntas (1).py
@@ -0,0 +1,64 @@
+"""
+Optimización usando gradiente descendente - Regresión polinomial
+-----------------------------------------------------------------------------------------
+
+En este laboratio se estimarán los parámetros óptimos de un modelo de regresión 
+polinomial de grado `n`.
+
+"""
+
+
+def pregunta_01():
+    """
+    Complete el código presentado a continuación.
+    """
+    # Importe pandas
+    import pandas as pd
+
+    # Importe PolynomialFeatures
+    from sklearn.preprocessing import PolynomialFeatures
+
+    # Cargue el dataset `data.csv`
+    data = pd.read_csv("data.csv")
+
+    # Cree un objeto de tipo `PolynomialFeatures` con grado `2`
+    poly = PolynomialFeatures(2)
+
+    # Transforme la columna `x` del dataset `data` usando el objeto `poly`
+    x_poly = poly.fit_transform(data[["x"]])
+
+    # Retorne x y y
+    return x_poly, data.y
+
+
+def pregunta_02():
+
+    # Importe numpy
+    import numpy as np
+
+    x_poly, y = pregunta_01()
+
+    # Fije la tasa de aprendizaje en 0.0001 y el número de iteraciones en 1000
+    learning_rate = 0.0001
+    n_iterations = 500
+ 
+    # Defina el parámetro inicial `params` como un arreglo de tamaño 3 con ceros
+    params = np.zeros(x_poly.shape[1])
+    for epoch in range(n_iterations):
+
+        # Compute el pronóstico con los parámetros actuales
+        y_pred = np.dot(x_poly, params)
+
+        # Calcule el error
+        error = [yt-yp for yt,yp in zip(y,y_pred)]
+        # Calcule el gradiente
+        grad_w2 = -2*sum([error*x_valor[0] for error, x_valor in zip(error,x_poly)])
+        grad_w1 = -2*sum([error*x_valor[1] for error, x_valor in zip(error,x_poly)])
+        grad_w0 = -2*sum([error*x_valor[2] for error, x_valor in zip(error,x_poly)])
+        
+        gradient = np.array([grad_w2,grad_w1,grad_w0])
+
+        # Actualice los parámetros
+        params = params - learning_rate * gradient
+
+    return params
